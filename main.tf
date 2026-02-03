11278a8d9bb11a474775fd09a43e8edafde04cb6
himanshu <rathihimanshu@live.com>
2026-02-03 16:04:19 +0530

    done

--- main.tf
+++ main.tf
@@ -1,5 +1,33 @@
-resource "google_storage_bucket" "gcs1" {
-    name     = "himanshu-unique-bucket-name-12345"
-    location = var.region
+# resource "google_storage_bucket" "gcs1" {
+#     name     = "himanshu-unique-bucket-name-12345"
+#     location = var.region
   
+# }
+
+resource "google_compute_instance" "vm1" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
 }
